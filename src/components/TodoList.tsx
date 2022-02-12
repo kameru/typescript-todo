52dc07444247a1a58196fe0821bcee59e047c8d7
kameru <kameru1208@gmail.com>
2022-02-12 17:26:45 +0900

    완료된 항목 스타일 추가, 사용하지 않는 코드 제거

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -2,11 +2,10 @@ import * as React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { RootState, ToggleComplete } from '../store/todoRedux';
 import { TodoItem } from '../type';
-import { getTodoList, setTodoList } from '../utils/localStore';
+import { setTodoList } from '../utils/localStore';
 import TodoItemComponent from './TodoItem';
 
 const TodoList:React.FC = () => {
-    const list = getTodoList();
     const todoList = useSelector((state: RootState) => (state.todoList));
     const dispatch = useDispatch();
     
