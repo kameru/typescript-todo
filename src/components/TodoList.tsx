176ce6270030c69f4b81407d2aebd89d5988870a
kameru <kameru1208@gmail.com>
2022-02-12 17:18:39 +0900

    redux로 할일  완료여부 처리

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
-import { useSelector } from 'react-redux';
-import { RootState } from '../store/todoRedux';
+import { useSelector, useDispatch } from 'react-redux';
+import { RootState, ToggleComplete } from '../store/todoRedux';
 import { TodoItem } from '../type';
 import { getTodoList, setTodoList } from '../utils/localStore';
 import TodoItemComponent from './TodoItem';
@@ -8,10 +8,13 @@ import TodoItemComponent from './TodoItem';
 const TodoList:React.FC = () => {
     const list = getTodoList();
     const todoList = useSelector((state: RootState) => (state.todoList));
+    const dispatch = useDispatch();
     
     const onCheck = (e:React.ChangeEvent<HTMLInputElement>, index: number):void => {
         const newList = [...todoList];
-        newList[index].isCompleted = e.target.checked;
+        const isCompleted = e.target.checked;
+        dispatch(ToggleComplete(index, isCompleted));
+        newList[index].isCompleted = isCompleted;
         setTodoList(newList);
     }
 
